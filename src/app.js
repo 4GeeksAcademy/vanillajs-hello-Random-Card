2c4d17b0487d81ffdb36111179df75a8683484d7
Nandito54 <nanditojose54@gmail.com>
2024-12-11 15:37:37 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const iconos = [" ♦ ", " ♥  ", " ♣  ", " ♠  "];
+  let valoresRandom = Math.floor(Math.random() * valores.length);
+  let iconosRandom = Math.floor(Math.random() * iconos.length);
+
+  document.querySelector(".top").textContent = iconos[iconosRandom];
+  document.querySelector(".bottom").textContent = iconos[iconosRandom];
+  document.querySelector(".medium").textContent = valores[valoresRandom];
 };
